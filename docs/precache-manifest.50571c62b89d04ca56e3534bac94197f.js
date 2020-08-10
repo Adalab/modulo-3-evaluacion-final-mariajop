70ef4f2ebd61b2f3a376056c1e844d000e2bea20
mariajop <mariajops@gmail.com>
2020-08-10 11:35:53 +0200

    publishing to github

---
+++ docs/precache-manifest.50571c62b89d04ca56e3534bac94197f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e131c30605becc42b0d240778c3b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "851b79146fd93b75f859",
+    "url": "/static/css/main.40d72a2f.chunk.css"
+  },
+  {
+    "revision": "72e722e5a02adbf8eb66",
+    "url": "/static/js/2.549b0492.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.549b0492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851b79146fd93b75f859",
+    "url": "/static/js/main.9fa545d2.chunk.js"
+  },
+  {
+    "revision": "5dbe6bd8fc00797c75d1",
+    "url": "/static/js/runtime-main.f30f5965.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/Rick_and_Morty_-_logo_(English).75509f2e.png"
+  },
+  {
+    "revision": "1fa59f84077037b72f2cb70ab6a1c3d7",
+    "url": "/static/media/background.1fa59f84.png"
+  }
+]);
